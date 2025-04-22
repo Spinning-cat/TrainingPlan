b1a777465f0196c3e492166d848b779e366ee36f
Spinning-cat <15519651+Spinning-cat@user.noreply.gitee.com>
2025-04-22 19:54:52 +0800

    取消优先级，设置5类课程

--- Course.h
+++ Course.h
@@ -10,7 +10,7 @@ struct Course {
     string name;              // 课程名称
     double credit;            // 学分
     int hours;                // 课时
-    int priority;             // 课程优先级（1:公共基础/实践必修；2:专业基础；3:专业选修；4:实践选修）
+    int type;             // 课程类型（1:公共基础；2:专业基础；3:专业选修；4:实践必修； 5:实践选修）
     vector<string> prerequisites; // 先修课程列表
 };
 #endif // COURSE_H
